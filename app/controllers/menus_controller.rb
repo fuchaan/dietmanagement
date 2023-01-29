e506f56226fa904cf253aeedabb040b66d4a5ed2
fuchaan <miyamoto.fuma1@gmail.com>
2023-01-29 09:50:17 +0900

    一覧と投稿

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,18 @@
 class MenusController < ApplicationController
+  def index
+    @menus = Menu.all.order(created_at: 'DESC')
+  end
+
+  def new
+    @menu = Menu.new 
+  end
+
+  def create
+    Menu.create(menu_params)
+  end
+
+  private
+  def menu_params
+    params.require(:menu).permit(:name, :image, :calorie)
+  end
 end
---
+++ app/models/menu.rb
@@ -0,0 +1,4 @@
+class Menu < ApplicationRecord
+  has_one_attached :image
+  validates :text, presence: true
+end
