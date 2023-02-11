5b0e65689efc7d48264ca210de16212578dff6ee
fuchaan <miyamoto.fuma1@gmail.com>
2023-02-11 23:22:18 +0900

    マイページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @menus = Menu.where(user_id:params[:id]).order(created_at: 'DESC')
+    @menus = Menu.where(user_id:params[:id])
   end
 end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,6 +1,7 @@
 class Menu < ApplicationRecord
-  has_one_attached :image
-  belongs_to :user
   validates :name, presence: true
   validates :calorie, presence: true
+
+  has_one_attached :image
+  belongs_to :user
 end
